41ce7f918fc5eea0cf6e1cbca5223dd8370bf69c
Jakob <shavinski.jakob@gmail.com>
2023-12-11 19:24:49 -0800

    Finish half the lecture for tonight, left off at meow.c

--- week-1/compare.c
+++ week-1/compare.c
@@ -1,3 +1,22 @@
 #include <stdio.h>
-#include "libcs50/cs50.h"
+#include <cs50.h>
 
+int main(void)
+{
+    int x = get_int("What's x? ");
+    int y = get_int("What's y? ");
+
+    if (x < y)
+    {
+        printf("X is less than y\n");
+    }
+    else if (x > y)
+    {
+        printf("X is not less than Y\n");
+    }
+    else 
+    {
+        printf("X is equal to Y\n");
+    }
+
+}
--- week-1/hello.c
+++ week-1/hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
+// need to compile doing this: gcc hello.c -o hello -lcs50 
 
 int main(void)
 {
