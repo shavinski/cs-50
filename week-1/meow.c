8d6f8694f2189a331f3cc2fb8b3d07d0607bcdb5
Jakob <shavinski.jakob@gmail.com>
2023-12-18 18:18:12 -0800

    Take break work on customizign CLI

---
+++ week-1/meow.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <cs50.h>
+
+// need to compile doing this: gcc hello.c -o hello -lcs50 
+
+int main(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        printf("meow\n");
+    }
+}
